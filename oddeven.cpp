9c66da7be77369212ec967ccc6b119592cf94bad
kavita <kavitaraghvani2@gmail.com>
2025-10-14 14:05:12 +0530

    first commit

---
+++ oddeven.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {1,2,3,4,5,6};
+    int even=0;
+    int odd=0;
+
+    for(int i=0; i<6; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd << endl;
+}
